8c3243679fd591b0b4dfcf258ad55bf67df324a2
oflaned <misha122000@icloud.com>
2022-08-08 21:28:35 +0300

    add Name to db and fix /rm command

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 import TelegramApi from "node-telegram-bot-api"
 import dot from 'dotenv'
-import { ssvStatus } from './models/ssvStatus.js'
-import { sendRequest } from './check.js'
+import * as botCommands from './bot/commands.js'
 
-dot.config()
-const bot = new TelegramApi(process.env.BOT_TOKEN, {polling:true})
+await dot.config()
+let token = process.env.BOT_TOKEN
+export const bot = await new TelegramApi(process.env.BOT_TOKEN, {polling:true})
 
 
 export const start = () => {
@@ -29,71 +29,38 @@ export const start = () => {
     ])
 
     bot.on('message', async msg=>{
-        const text = msg.text;
-        const curentChatId = msg.chat.id;
-        if (!text) return
+        let text = msg.text
+        if (text === undefined) { return }
+        text = text.toLocaleLowerCase()
+        const curentChatId = msg.chat.id
 
-        if (text === '/start') { bot.sendMessage(curentChatId, `Hi, @${msg.chat.username}\n\n Here you can add your ssv node to track status\n by command <code>/add addressOfNode</code>`, {parse_mode:'HTML'}) }
 
-        if (text === '/add') { return bot.sendMessage(curentChatId,`Please write  <b>address of node</b>  after <code>/add</code> `, {parse_mode:'HTML'}) }
-
-        if (text.startsWith('/add ')){
-            let addressFromMessage = msg.text.replace('/add ','')
-            if(addressFromMessage.length != 64) { return bot.sendMessage(curentChatId,`You are write a wrong address`) }
-            sendRequest(process.env.SSV_URL+addressFromMessage).then(res => {
-                ssvStatus.findOne({address: addressFromMessage}).then(response => {
-                    if (response === null) {
-                        ssvStatus.create({chatId:curentChatId, address: addressFromMessage}, (err, res) => { if(err) throw err })
-                        return bot.sendMessage(curentChatId,`Node was successfully added`)
-                    }
-                    if (response.chatId.indexOf(curentChatId) != -1) { return bot.sendMessage(curentChatId,`Node was already added`) } 
-
-                    let newChaitIdArray = response.chatId
-                    newChaitIdArray.push(curentChatId)
-                    ssvStatus.findOneAndUpdate({address: addressFromMessage},{chatId: newChaitIdArray }, (err, res) => { if(err) throw err })
-                    return bot.sendMessage(curentChatId,`Node was successfully added`)
-                })
-            }).catch(err => { return bot.sendMessage(curentChatId,`You are write a wrong address`) })
+        if ( text === '/start' ) { 
+            bot.sendMessage(curentChatId, `Hi, @${msg.chat.username}\n\n Here you can add your ssv node to track status\n by command <code>/add addressOfNode</code>`, {parse_mode:'HTML'}) 
         }
 
-        if(text === '/nodes') {
-            ssvStatus.find({chatId: curentChatId}, (err, nodes) => {
-                var str = ``
-                for(let i = 0; i < nodes.length;i++){
-                    if(nodes[i]['status']=='Active') {
-                        str += `<b>SSV Node ${i+1}:</b> <code>${nodes[i]['address']}</code>    <b>${nodes[i]['status']}</b>\u2705\n\n`
-                    } else { str += `<b>SSV Node ${i+1}:</b> <code>${nodes[i]['address']}</code>    <b>${nodes[i]['status']}</b>\u274c\n\n` }
-                }
-                return bot.sendMessage(curentChatId, str, {parse_mode:'HTML'})
-            })
+        if ( text.startsWith('/add') ) {
+            botCommands.add(curentChatId, text)
         }
 
-        if (text === '/rm') { return bot.sendMessage(curentChatId,`Please write  <b>address of node</b>  after <code>/rm</code> `, {parse_mode:'HTML'}) }
+        if ( text.startsWith('/nodes') ) {
+            botCommands.nodes(curentChatId, text)
+        }
 
-        if(text.startsWith('/rm ')) {
-            let addressFromMessage = msg.text.replace('/rm ','')
-            if(addressFromMessage.length != 64) { return bot.sendMessage(curentChatId,`You are wrote a wrong address`) }
-            ssvStatus.findOne({address: addressFromMessage}).then (res => {
-                if (res === null) {
-                    return bot.sendMessage(curentChatId,`You are wrote a wrong address`)
-                }
-                let newChaitIdArray = res.chatId
-                delete newChaitIdArray[newChaitIdArray.indexOf(curentChatId)]
-                ssvStatus.findOneAndUpdate({address: addressFromMessage}, {chatId: newChaitIdArray}, (err, res) => { if(err) throw err })
-                return bot.sendMessage(curentChatId,`<b>Node successfully deleted</b>`, {parse_mode:'HTML'})
-                
-            }).catch(err => { console.log(err) }) 
+        if ( text.startsWith('/rm') ) {
+            botCommands.rm(curentChatId, text)
         }
 
     })
 }
 
-export function alertNode(chatIds, status, address){
-    console.log(chatIds)
+export function alertNode(chatIds, status, address, name){
+    console.log(`msg to ${chatIds}`)
+    console.log(name)
     chatIds.forEach(id => {
         if (status === 'Active')
-            bot.sendMessage(id, `<b>Status of node:</b> <code>${address}</code> <b>    is ${status}</b>\u2705`, {parse_mode:'HTML'})
+            bot.sendMessage(id, `<b>Status of ${name} node: </b> <code>${address}</code> <b>    is ${status}</b>\u2705`, {parse_mode:'HTML'})
         else
-            bot.sendMessage(id, `<b>Status of node:</b> <code>${address}</code> <b>    is ${status}</b>\u274c`, {parse_mode:'HTML'})
+            bot.sendMessage(id, `<b>Status of ${name} node: </b> <code>${address}</code> <b>    is ${status}</b>\u274c`, {parse_mode:'HTML'})
     });
 }
